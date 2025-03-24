f1f9d014a0497178ec2f3e8e05ea7967138e4eb1
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2025-03-25 00:55:42 +0530

    style: changed mt-8 to pt-8 in layout of docs

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="md:w-[95%] h-[96vh] mx-auto flex  gap-4 md:gap-8">
       <DocsSidebar />
       <main className="flex-1 md:px-8 w-full overflow-scroll no-scrollbar">
-        <div className="mt-8 h-full overflow-scroll no-scrollbar">
+        <div className="pt-8 h-full overflow-scroll no-scrollbar">
           {children}
         </div>
       </main>
